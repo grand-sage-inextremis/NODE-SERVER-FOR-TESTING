f04043defca26ed951e34dc9c7d29c7f465a19dd
Grand Sage InExtremis <grand.sage.inextremis@gmail.com>
2025-09-09 19:32:22 +0200

    feat: add a server

--- dist/server/index.js
+++ dist/server/index.js
@@ -1,3 +1,19 @@
-const greeting = "Hello World!";
+import { createServer } from 'http';
 
-console.log(greeting);
+
+
+const HOSTNAME = 'localhost';
+const PORT = 8000;
+
+
+
+const server = createServer(function (req, res)
+{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World!\n');
+});
+
+
+
+server.listen(PORT, HOSTNAME, () => console.log(`Server running on http://${HOSTNAME}:${PORT}/...`));
